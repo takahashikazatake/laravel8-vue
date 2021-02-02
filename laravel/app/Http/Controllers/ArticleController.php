e0f63762e04a4f02e93f9ecc390e3949c6157525
pieru <aran0204.ambro@icloud.com>
2021-02-02 16:54:38 +0900

    Articleモデルとそれをホーム画面で表示する機能の作成

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -9,39 +11,7 @@ class ArticleController extends Controller
     //==========ここから追加==========
     public function index()
     {
-        // ダミーデータ
-        $articles = [
-            (object) [
-                'id' => 1,
-                'title' => 'アパパパ',
-                'body' => 'hogehoge',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'あらん',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
+        $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
     }

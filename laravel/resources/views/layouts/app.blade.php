97e6a15f6999293e64cbb12b55bca33aae005333
Kento Nakamura <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-02-03 18:01:01 +0900

    classの削除

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="app">
+    <div>
         <nav class="navbar navbar-expand-md navbar-dark shadow-sm blue-gradient">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">

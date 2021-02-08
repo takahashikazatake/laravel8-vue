82965e59a7df24215a5e9995b1bfb1f8fa7051c5
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-02-08 12:49:57 +0900

    モーダルにフォームを作成

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -9,17 +9,24 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">好きなことをつぶやこう</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <div class="form-group">
+                            <label for="TweetTitle">タイトル</label>
+                            <input class="form-control" type="text" id="TweetTitle">
+                        </div>
+                        <div class="form-group">
+                            <label for="Textarea">ツイート</label>
+                            <textarea class="form-control" id="Textarea" rows="5"></textarea>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-primary">投稿する</button>
                     </div>
                 </div>
             </div>

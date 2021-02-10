705716c32acb49fecee44e612f35e74558d3ee7e
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-02-10 17:15:47 +0900

    postButtonのコンポーネント化

---
+++ laravel/resources/js/components/PostButtonComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="d-flex flex-row-reverse">
+    <button
+      type="button"
+      class="btn btn-primary btn-sm"
+      data-toggle="modal"
+      data-target="#exampleModal"
+    >投稿</button>
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">好きなことをつぶやこう</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="TweetTitle">タイトル</label>
+              <input class="form-control" type="text" id="TweetTitle" />
+            </div>
+            <div class="form-group">
+              <label for="Textarea">ツイート</label>
+              <textarea class="form-control" id="Textarea" rows="5"></textarea>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">投稿する</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

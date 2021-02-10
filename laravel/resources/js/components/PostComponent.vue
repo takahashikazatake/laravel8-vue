8f50070841e2a8e2ba3bd082387ac0d0022e5082
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-02-10 21:07:32 +0900

    親コンポーネントから子コンポーネントへのデータ受け渡し

--- laravel/resources/js/components/PostComponent.vue
+++ laravel/resources/js/components/PostComponent.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="card mb-2">
     <div class="card-body">
-      <div class="font-weight-bold">{{ user }}</div>
-      <div class="card-title mt-4">{{ tweet }}</div>
-      <div class="card-text mt-3">投稿日時 {{ postDate }}</div>
+      <div class="font-weight-bold">{{ post.user }}</div>
+      <div class="card-title mt-4">{{ post.tweet }}</div>
+      <div class="card-text mt-3">投稿日時 {{ post.createdAt }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["post"],
   data: function() {
-    return {
-      user: "User Name",
-      tweet:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      postDate: "2021年02月2日"
-    };
+    return {};
   }
 };
 </script>
--- laravel/resources/js/components/PostIndexComponent.vue
+++ laravel/resources/js/components/PostIndexComponent.vue
@@ -8,7 +8,7 @@
         data-target="#exampleModal"
       >投稿</button>
     </div>
-    <post-component></post-component>
+    <post-component v-for="post in posts" :key="post.id" :post="post"></post-component>
     <div
       class="modal fade"
       id="exampleModal"
@@ -50,6 +50,26 @@ import PostComponent from "./PostComponent";
 export default {
   components: {
     PostComponent
+  },
+  data: function() {
+    return {
+      posts: [
+        {
+          id: 1,
+          user: "User Name",
+          tweet:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          createdAt: "2021年02月12日"
+        },
+        {
+          id: 2,
+          user: "User Name2",
+          tweet:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          createdAt: "2021年02月16日"
+        }
+      ]
+    };
   }
 };
 </script>

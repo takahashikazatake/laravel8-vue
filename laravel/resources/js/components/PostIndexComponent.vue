530a8565182fa755753ca83556e3e30c0e02e9e5
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-02-18 11:42:53 +0900

    Vue v-onが効かないエラーの解決

--- laravel/resources/js/components/PostIndexComponent.vue
+++ laravel/resources/js/components/PostIndexComponent.vue
@@ -19,30 +19,14 @@ export default {
   props: ["userName"],
   data: function() {
     return {
-      posts: [
-        {
-          id: 1,
-          user: "User Name",
-          tweet:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          createdAt: "2021年02月12日"
-        },
-        {
-          id: 2,
-          user: "User Name2",
-          tweet:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          createdAt: "2021年02月16日"
-        }
-      ],
+      posts: [],
       name: this.userName.name,
       newTweet: "",
-      nextId: 3
+      nextId: 1
     };
   },
   methods: {
     addNewPost: function() {
-      console.log("hello");
       this.posts.push({
         id: this.nextId++,
         user: this.name,
@@ -50,9 +34,6 @@ export default {
         createdAt: "2021/02/12"
       });
       this.newTweet = "";
-    },
-    hello: function() {
-      console.log("hello");
     }
   }
 };

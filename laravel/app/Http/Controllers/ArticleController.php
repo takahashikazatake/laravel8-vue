3379536e17ca850cd11b6a3fdb37686388cdbcfc
kento0805-naka <kentz.eva.a.r.xxx@gmail.com>
2021-02-25 13:14:05 +0900

    投稿一覧の表示順の変更

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -13,7 +13,7 @@ class ArticleController extends Controller
     }
 
     public function index() {
-        $articles = Article::all();
+        $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
     }
 

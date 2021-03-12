8a73822de052edff0e11c90387116f7ddb581a4a
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-03-12 10:13:30 +0900

    投稿日時の表示

--- laravel/resources/views/articles/post.blade.php
+++ laravel/resources/views/articles/post.blade.php
@@ -5,7 +5,7 @@
     <div class="card-body">
         <div class="font-weight-bold">{{ $article->user->name }}</div>
         <div class="card-title mt-4">{{ $article->body }}</div>
-        <div class="card-text mt-3">投稿日時 {{ $article->createdAt }}</div>
+        <div class="card-text mt-3">投稿日時 {{ $article->created_at }}</div>
     </div>
     </div>
 </div>

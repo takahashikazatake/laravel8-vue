759b651e618d296c6ede5ea161148106766d85e4
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-03-14 19:01:28 +0900

    インデントの修正

--- laravel/resources/views/articles/post.blade.php
+++ laravel/resources/views/articles/post.blade.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="card mb-2">
-    <div class="card-header d-flex flex-row-reverse">
-    </div>
-    <div class="card-body">
-        <div class="font-weight-bold">{{ $article->user->name }}</div>
-        <div class="card-title mt-4">{{ $article->body }}</div>
-        <div class="card-text mt-3">投稿日時 {{ $article->created_at }}</div>
-    </div>
+        <div class="card-header d-flex flex-row-reverse">
+        </div>
+        <div class="card-body">
+            <div class="font-weight-bold">{{ $article->user->name }}</div>
+            <div class="card-title mt-4">{{ $article->body }}</div>
+            <div class="card-text mt-3">投稿日時 {{ $article->created_at }}</div>
+        </div>
     </div>
 </div>
 

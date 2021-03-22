60eb77de751fb1ed624b2b87470129a82d65d5b7
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-03-22 12:17:00 +0900

    Article Factoryの作成

---
+++ laravel/database/factories/ArticleFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $user = User::inRandomOrder()->first();
+        
+        return [
+            'id' => 1,
+            'body' => 'Hello',
+            'user_id' => $user->id,
+        ];
+    }
+}

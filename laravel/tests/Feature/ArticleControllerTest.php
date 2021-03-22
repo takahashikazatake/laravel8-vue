7605f431e81c6a71a088f4c18e83eedf8858c11e
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-03-22 12:22:14 +0900

    記事作成・削除のテスト

--- laravel/tests/Feature/ArticleControllerTest.php
+++ laravel/tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,10 +12,34 @@ class ArticleControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void {
+
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+    }
+
     public function testIndex() {
         
         $response = $this->get(route('article.index'));
 
         $response->assertStatus(200)->assertViewIs('articles.index');
+    }
+
+    public function testStore() {
+
+        $this->withoutMiddleware()->post(route('article.store'), [
+            'body' => 'Hello',
+            'user_id' => $this->user->id
+        ])->assertStatus(302);
+    }
+
+    public function testDestory() {
+
+        $post = Article::factory()->create();
+        $url = '/article/' . strval($post->id);
+
+        $this->withoutMiddleware()->delete($url)->assertStatus(200);
     }
 }

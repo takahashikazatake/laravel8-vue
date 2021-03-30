56d65058474ba9476e23951db0b08212c65fa934
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-03-30 10:11:20 +0900

    投稿のスタイル変更

--- laravel/resources/js/components/PostCard.vue
+++ laravel/resources/js/components/PostCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card mb-2">
+  <!-- <div class="card mb-2">
     <div class="card-header d-flex flex-row-reverse">
       <span @click="del" v-show="post.userId === this.authId">&times;</span>
     </div>
@@ -15,7 +15,22 @@
       <div class="card-title mt-4">{{ post.tweet }}</div>
       <div class="card-text mt-3">投稿日時 {{ post.createdAt }}</div>
     </div>
-  </div>
+  </div>-->
+  <v-card class="mx-auto mb-3" max-width="600">
+    <v-card-title>
+      <v-icon large left>mdi-account-circle</v-icon>
+      <span class="title font-weight-light">{{ post.user }}</span>
+    </v-card-title>
+    <v-card-text class="headline font-weight-bold">{{ post.tweet }}</v-card-text>
+    <v-card-text class="headline">投稿日時 {{ post.createdAt }}</v-card-text>
+    <v-card-actions class="d-flex flex-row-reverse pr-4">
+      <v-icon right @click="del" v-show="post.userId === this.authId">mdi-delete</v-icon>
+      <v-btn @click="like" icon color="pink">
+        <v-icon>mdi-heart</v-icon>
+        <span>2</span>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -29,6 +44,9 @@ export default {
   methods: {
     del: function() {
       this.$emit("del");
+    },
+    like: function() {
+      alert("Like it!");
     }
   }
 };
